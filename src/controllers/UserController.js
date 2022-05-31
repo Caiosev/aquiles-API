e79167758e36a4e0dc60a49b5ea3847807f889bf
caiosev <kaiosev9@gmail.com>
2022-05-31 06:47:24 -0300

    CRUD users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,70 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id nao enviado'],
+        });
+      }
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuario nao existe'],
+        });
+      }
+      const novoUser = await user.update(req.body);
+      return res.json(novoUser);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Id nao enviado'],
+        });
+      }
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuario nao existe'],
+        });
+      }
+      await user.destroy();
+      return res.json({ Deletado: true });
+    } catch (e) {
+      return res.json(null);
     }
   }
 }
--- src/models/User.js
+++ src/models/User.js
@@ -42,6 +42,7 @@ export default class User extends Model {
       },
     }, { sequelize });
     this.addHook('beforeSave', async (user) => {
+      if (!user.password) return;
       user.password_hash = await bcryptjs.hash(user.password, 8);
     });
     return this;
