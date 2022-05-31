0347d7f7ba58ab856ecf09cf72ca91575568cec4
caiosev <kaiosev9@gmail.com>
2022-05-31 11:49:09 -0300

    Rota alunos e migration email aluno unique

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Anaxímenes ',
-      sobrenome: 'de Mileto',
-      email: 'OSegundoPapiro@gmail.com',
-      idade: 2610,
-      peso: '90',
-      altura: 1.75,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 export default new HomeController();
