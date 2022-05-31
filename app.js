0858f463f3c978b62f527f35bfd2cfce451d2e63
caiosev <kaiosev9@gmail.com>
2022-05-31 09:28:16 -0300

    Gerando Token para User

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
+import tokenRoutes from './src/routes/TokenRoutes';
 import './src/database';
 
 dotenv.config();
@@ -25,6 +26,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users', userRoutes);
+    this.app.use('/tokens', tokenRoutes);
   }
 }
 
