b7e8a0a6cda2d6ca3767c4c6b192181e1638dd47
caiosev <kaiosev9@gmail.com>
2022-06-01 16:08:20 -0300

    corrigindo upload de fotos

--- src/models/Foto.js
+++ src/models/Foto.js
@@ -27,7 +27,7 @@ export default class Foto extends Model {
       url: {
         type: Sequelize.VIRTUAL,
         get() {
-          return `${appConfig.url}/images/${this.getDataValue('filename')}`;
+          return `uploads/images/${this.getDataValue('filename')}`;
         },
       },
     }, { sequelize });
