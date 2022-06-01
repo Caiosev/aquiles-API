c3e89706ee595f113219dfe224db26298f4a6abc
caiosev <kaiosev9@gmail.com>
2022-06-01 06:23:10 -0300

    ajustes finais e arquivos estaticos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
 import dotenv from 'dotenv';
 import express from 'express';
+import { resolve } from 'path';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/TokenRoutes';
@@ -24,6 +25,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'uploads')));
   }
 
   routes() {
